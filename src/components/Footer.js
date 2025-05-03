6d7d14fb35b57feaca0b8442cf2bfbbea91f764c
Yuss76A <yuse_91yus@hotmail.com>
2025-05-03 17:40:02 +0200

    Fix footer and reduce size of the footer, change social link to just icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,20 +10,32 @@ const Footer = () => {
                 <Link to="/contact" className={styles['footer-link']}>Contact Us</Link>
             </nav>
             <div className={styles['social-links']}>
-                <a href="https://www.instagram.com/YourInstagramHandle" target="_blank" rel="noopener noreferrer" className={styles['footer-link']}>
-                    <i className="fab fa-instagram"></i> Instagram
+                <a href="https://www.instagram.com/YourInstagramHandle" 
+                   target="_blank" 
+                   rel="noopener noreferrer"
+                   aria-label="Instagram">
+                    <i className="fab fa-instagram"></i>
                 </a>
-                <a href="https://www.facebook.com/YourFacebookPage" target="_blank" rel="noopener noreferrer" className={styles['footer-link']}>
-                    <i className="fab fa-facebook-f"></i> Facebook
+                <a href="https://www.facebook.com/YourFacebookPage" 
+                   target="_blank" 
+                   rel="noopener noreferrer"
+                   aria-label="Facebook">
+                    <i className="fab fa-facebook-f"></i>
                 </a>
-                <a href="https://www.tiktok.com/@YourTikTokHandle" target="_blank" rel="noopener noreferrer" className={styles['footer-link']}>
-                    <i className="fab fa-tiktok"></i> TikTok
+                <a href="https://www.tiktok.com/@YourTikTokHandle" 
+                   target="_blank" 
+                   rel="noopener noreferrer"
+                   aria-label="TikTok">
+                    <i className="fab fa-tiktok"></i>
                 </a>
-                <a href="https://twitter.com/YourTwitterHandle" target="_blank" rel="noopener noreferrer" className={styles['footer-link']}>
-                    <i className="fab fa-twitter"></i> Twitter
+                <a href="https://twitter.com/YourTwitterHandle" 
+                   target="_blank" 
+                   rel="noopener noreferrer"
+                   aria-label="Twitter">
+                    <i className="fab fa-twitter"></i>
                 </a>
             </div>
-            <p>© 2025 Happy Rental. All rights reserved.</p>
+            <p className={styles['copyright']}>© 2025 Happy Rental. All rights reserved.</p>
         </footer>
     );
 };
